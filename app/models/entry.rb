1aae52445f806fe00126d49098abb30af432a9c6
ducminhvu <vu.minh.duc@framgia.com>
2015-06-15 23:10:59 +0700

    login logout

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,6 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, length: { maximum: 250 }
   validates_presence_of :title, :content, :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :entries
-  has_many :comments
+  has_many :entries, dependent: :destroy
+  has_many :comments, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
