b3542663f05758b54508b10bc0f389475584441a
Vu Minh Duc <vu.minh.duc@framgia.com>
2015-06-16 10:31:33 +0700

    follow user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   def new
     @user = User.new
   end
@@ -22,6 +24,20 @@ class UsersController < ApplicationController
 
   def index
     @users = User.paginate(page: params[:page])
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ User.create!(name:  "Vu Minh Duc",
              password:              "abc123",
              password_confirmation: "abc123")
 
-99.times do |n|
+30.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -14,8 +14,16 @@ User.create!(name:  "Vu Minh Duc",
 end
 
 users = User.order(:created_at).take(6)
-50.times do
+20.times do
   title = Faker::Lorem.sentence(1)
   content = Faker::Lorem.sentence(6)
   users.each { |user| user.entries.create!(content: content, title: title) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..10]
+followers = users[3..15]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
